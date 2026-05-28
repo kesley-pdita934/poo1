af909d674e9e168d758ceef10a64d7bbf6017f7c
kesley-pdita934 <kesley.amorimpdita934@pditabira.com.br>
2026-05-27 23:11:44 -0300

    Add files via upload

--- modulo2/Autor.java
+++ modulo2/Autor.java
@@ -1,16 +1,31 @@
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Autor extends Pessoa {
     private String nacionalidade;
+    private boolean ehUsuario;
+    
+    
+    private PublicavelInterface estrategiaPublicacao;
 
     public Autor(String nome, String nacionalidade) {
         super(nome);
         this.nacionalidade = nacionalidade;
+        this.ehUsuario = false;
+    }
+
+    public Autor(String nome, String nacionalidade, boolean ehUsuario) {
+        super(nome);
+        this.nacionalidade = nacionalidade;
+        this.ehUsuario = ehUsuario;
     }
 
     public String getNacionalidade() { return nacionalidade; }
     public void setNacionalidade(String nacionalidade) { this.nacionalidade = nacionalidade; }
+
+    public boolean isEhUsuario() { return ehUsuario; }
+    public void setEhUsuario(boolean ehUsuario) { this.ehUsuario = ehUsuario; }
 
     public List<Livro> getObrasPublicadas() {
         return getLivros();
@@ -24,5 +39,16 @@ public class Autor extends Pessoa {
             }
         }
         return filtrados;
+    }
+    public void setEstrategiaPublicacao(PublicavelInterface estrategia) {
+        this.estrategiaPublicacao = estrategia;
+    }
+
+    public void publicar() {
+        if (this.estrategiaPublicacao != null) {
+            this.estrategiaPublicacao.publicar();
+        } else {
+            System.out.println("Erro: Nenhuma estratégia de publicação foi definida para este autor.");
+        }
     }
 }
